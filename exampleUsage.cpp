437c634f67d8cd3650415eaf507aaefbc0999128
Martin <134869415+Martin000N@users.noreply.github.com>
2024-07-29 00:15:47 +0200

    Update exampleUsage.cpp
    simplified code. made it better

--- exampleUsage.cpp
+++ exampleUsage.cpp
@@ -1,69 +1,29 @@
 #include <vector>
-#include <any>
-#include <unordered_map>
 #include "test.hpp"
 #include <windows.h>
-#include <omp.h>
 
 int main(){
     int x_res=1500;
-    int y_res=400;
-    
-    std::unordered_map<int, std::any> test;
+	int y_res=400;
 
     statusIgre::igra stanje;
     stanje.dolociResolucijo(x_res, y_res);
 
     primitivi::objekti objekt;
 
-    int moveX=0;
-    int moveY=0;
-    float zoom=10.0f;
+    renderanje::bufferArray buffer(x_res, y_res);
+    renderanje::zBufferArray zBuffer(x_res, y_res);
 
-    int st_pikslov=y_res*x_res+y_res;
-    char original[st_pikslov];
- 
-    float fakt=1.0f;
-    float fakt2=1.0f;
+    objekt+=(nalagalnik::nalaganjeTock::naloziOBB("C:\\Users\\marti\\Desktop\\scene\\scene.obj"));
+    objekt+=(nalagalnik::nalaganjeTock::naloziOBB("C:\\Users\\marti\\Desktop\\www\\scene.obj"));
 
-    renderanje::projekcija::pocistiZaslon();
-    memset(original, ' ', sizeof(original));
-    objekt+=(nalagalnik::nalaganjeTock::naloziOBB("C:\\Users..."));
-    objekt+=(nalagalnik::nalaganjeTock::naloziOBB("C:\\Users..."));
+    renderanje::projekcija::hardResetZaslon();
 
     while (stanje.pridobiStatus()){
-        if (GetAsyncKeyState(65)&0x8000){
-            stanje.x(10.0f);
-        }
-        else if (GetAsyncKeyState(68)&0x8000){
-            stanje.x(-10.0f);
-        }
-        else if (GetAsyncKeyState (0x57)){
-		stanje.y(10.0f);
-	}
-	else if (GetAsyncKeyState (0x53)){
-		stanje.y(-10.0f);
-	}
-        else if (GetAsyncKeyState(84)&0x8000){
-            stanje.z(0.1f);
-        }
-        else if (GetAsyncKeyState(82)&0x8000) {
-            stanje.z(-0.1f);
-        }
-        else if(GetAsyncKeyState(69)&0x8000){
-            stanje.gzs(0.2f);
-        }
-        else if(GetAsyncKeyState(81)&0x8000){
-            stanje.gzs2(0.2f);
-        }
-        renderanje::projekcija::pocistiZaslon();
-        renderanje::projekcija::prikaz(objekt, stanje, original);
-        memset(original, ' ', sizeof(original));
-        
-        
 
-        branjeInputa::input::preverjanjeInputa(&stanje, &test, &test);
+        renderanje::projekcija::pocistiZaslon();
+        renderanje::projekcija::prikaz(objekt, stanje, buffer, zBuffer);
+        branjeInputa::input::preverjanjeInputa(&stanje);
         
-    }
-    
+    } 
 }
